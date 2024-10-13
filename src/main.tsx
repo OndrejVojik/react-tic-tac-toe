8f66dce861ea1b4df70a286d62edaf53c60938e5
OndrejVojik <vojik.cz@gmail.com>
2024-10-13 08:40:09 +0200

    router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Game from './Game.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Game />
+    <BrowserRouter basename="/react-tic-tac-toe">
+      <Game />
+    </BrowserRouter>
   </StrictMode>,
 )
