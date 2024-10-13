b84c3fb9bce1cc6c149b2709c8c23213b35bd812
OndrejVojik <vojik.cz@gmail.com>
2024-10-13 10:10:37 +0200

    6

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig(({ mode }) => {
-  // Load environment variables based on the current mode (development or production)
-  const env = loadEnv(mode, process.cwd(), '');
-
-  // Use the loaded environment variable to set the base path
-  const basePath = env.VITE_BASE_PATH || '/';
-
+  const isGithub = mode === 'production.github';
   return {
+    base: isGithub ? '/react-tic-tac-toe/' : '/',
     plugins: [react()],
-	base: basePath,
+    build: {
+      outDir: 'dist',
+    },
   };
 });
