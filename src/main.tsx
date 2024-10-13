6dbeaee992eb81eec5b550ea9c584a3df0c1fedb
OndrejVojik <vojik.cz@gmail.com>
2024-10-13 08:53:21 +0200

    router update 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Game from './Game.tsx'
-import { BrowserRouter } from 'react-router-dom'
-
-const basePath = import.meta.env.BASE_URL || "/react-tic-tac-toe";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={basePath}>
-      <Game />
-    </BrowserRouter>
+    <Game />
   </StrictMode>,
 )
